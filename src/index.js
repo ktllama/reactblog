6e516d8a2ca0faaac9ab0b5705b8001f10580cf2
Katie <costantinkp@gmail.com>
2022-02-08 12:46:12 -0500

    created components and main route switch

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { BrowserRouter, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Route path='/' component={App} />
-    </Router>
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
