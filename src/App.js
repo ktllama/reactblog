78400127c5bfc97c6850d285f0db0a49b5427d11
Katie <costantinkp@gmail.com>
2022-02-09 11:45:21 -0500

    created nav bar and header

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { useState, useEffect } from 'react';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header title="React Blog"/>
       <Nav />
         <Switch>
           <Route exact path="/">
