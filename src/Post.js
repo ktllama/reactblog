0f6865a132a69d9ad95f0b0be10c4be94ef3e2cb
Katie <costantinkp@gmail.com>
2022-02-09 12:03:35 -0500

    multiple post snippets on home linked to post page

--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Post = () => {
+const Post = ({ post }) => {
   return (
-    <div>Post</div>
+    <article className='post'>
+        <Link to={`/post/${post.id}`}>
+            <h2>{post.title}</h2>
+            <p className='postDate'>{post.datetime}</p>
+        </Link>
+        {/* this link will take us to the individual post */}
+        <p className='postBody'>{
+            (post.body).length <= 25 
+                ? post.body
+                : `${(post.body).slice(0,25)}...`
+        }</p>
+        {/* if the post has less than 25 char it will show all, otherwise just show 25 char snippet.. */}
+    </article>
   )
 }
 
