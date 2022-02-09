0509d414f69d3d2633bc2f57a2e9c7e62bad263c
Katie <costantinkp@gmail.com>
2022-02-09 12:41:29 -0500

    setting up and passing state for post title and body

--- src/App.js
+++ src/App.js
@@ -40,7 +40,14 @@ function App() {
   ])
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
+
   const history = useHistory();
+
+  const handleSubmit = () => {
+
+  }
 
   const handleDelete = (id) => {
     const postsList = posts.filter(post => post.id !== id);
@@ -59,7 +66,13 @@ function App() {
             <Home posts={posts} />
           </Route>
           <Route exact path="/post">
-            <NewPost />
+            <NewPost 
+              handleSubmit={handleSubmit}
+              postTitle={postTitle}
+              setPostTitle={setPostTitle}
+              postBody={postBody}
+              setPostBody={setPostBody}
+            />
           </Route>
           <Route path="/post/:id">
             <PostPage posts={posts} handleDelete={handleDelete} />
