16591f6a05c07071528a25e5820a2fbe9aced3d5
Katie <costantinkp@gmail.com>
2022-03-11 17:48:55 -0500

    new end point for data

--- src/api/posts.js
+++ src/api/posts.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //set base url
 export default axios.create({
-    baseURL: 'http://localhost:3500'
+    baseURL: 'https://my-json-server.typicode.com/ktllama/blogdata'
 });
 
 //when you take a project live you would change your base url to whatever url you have from your host
